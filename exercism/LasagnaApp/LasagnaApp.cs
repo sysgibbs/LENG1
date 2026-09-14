eab5795f11cc57642091feb5ef11ae56bae91c65
g <84513918+blowawayyourtroubles@users.noreply.github.com>
2026-09-14 17:30:00 -0400

    added second excersie

---
+++ exercism/LasagnaApp/LasagnaApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+class Lasagna
+{
+
+    public int ExpectedMinutesInOven() => 40;
+    public int RemainingMinutesInOven(int actualMinutesInOven) => 40 - actualMinutesInOven;
+
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
+
+    public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven) => PreparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+
+}
