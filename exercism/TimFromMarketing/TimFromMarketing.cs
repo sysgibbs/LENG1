d0402f3d05bac323f0caaf457aeba717c4d405ba
g <84513918+blowawayyourtroubles@users.noreply.github.com>
2026-09-15 17:59:25 -0400

    trim from marketing null operator

---
+++ exercism/TimFromMarketing/TimFromMarketing.cs
@@ -0,0 +1,11 @@
+﻿static class Badge
+{
+    public static string Print(int ? id, string name, string ? department)
+    {
+        string formattedDepart = (department ?? "OWNER").ToUpper();
+
+        string idPrefix = id != null ? $"[{id}] - " : "";
+
+        return $"{idPrefix}{name} - {formattedDepart}";
+    }
+}
