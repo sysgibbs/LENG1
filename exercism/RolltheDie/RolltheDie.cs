770e7cf81d622817c3fb3749aaabd782aa17ba54
g <84513918+blowawayyourtroubles@users.noreply.github.com>
2026-09-15 14:22:09 -0400

    6 ejercicios hechos

---
+++ exercism/RolltheDie/RolltheDie.cs
@@ -0,0 +1,8 @@
+﻿System.Random;
+public class Player
+{
+    Random random = new Random();
+    public int RollDie() => random.Next(1,19);
+
+    public double GenerateSpellStrength() => random.NextDouble() * 100;
+}
