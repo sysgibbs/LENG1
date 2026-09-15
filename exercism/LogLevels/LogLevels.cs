07d18508ba9f7dbd9fcf9f49e0623b479e9c29cb
g <84513918+blowawayyourtroubles@users.noreply.github.com>
2026-09-15 12:50:26 -0400

    4 exercise done

--- exercism/LogLevels/LogLevels.cs
+++ exercism/LogLevels/LogLevels.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static class LogLine
+{
+    public static string Message(string logLine)
+    {
+
+        int posicion = logLine.IndexOf(":");
+        return logLine.Substring(posicion + 1).Trim();
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        int inicio = logLine.IndexOf("[");
+        int final = logLine.IndexOf("]");
+
+        int cantidad = final - inicio;
+        string palabraExtraida = logLine.Substring(inicio, cantidad).ToLower();
+    }
+
+    public static string Reformat(string logLine)
+    {
+        return $"{Message(LogLine)} {LogLevel(logLine)}";
+    }
+}
