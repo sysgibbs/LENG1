c7fbcae313f4c5c7c5d9bd179e0f183957bfafe9
g <84513918+blowawayyourtroubles@users.noreply.github.com>
2026-09-17 19:43:53 -0400

    added the program files to run the code

---
+++ exercism/CarsAssemble/AssemblyLine.cs
@@ -0,0 +1,9 @@
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(AssemblyLine.SuccessRate(5));
+        Console.WriteLine(AssemblyLine.ProductionRatePerHour(5));
+        Console.WriteLine(AssemblyLine.WorkingItemsPerMinute(5));
+    }
+}
